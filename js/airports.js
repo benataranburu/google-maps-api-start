4c3d882d67327aa482c1a376b9aace14aa187155
Beñat Aranburu <baranburu@gmail.com>
2018-05-31 21:01:45 +0200

    Airport list cleaned

--- js/airports.js
+++ js/airports.js
@@ -10,11 +10,12 @@
 var airportData = [
   {
     "aper": 85.1,
-    "code": "000",
+    "code": "ABC",
     "dper": 85.7,
     "airport": "TEST AIRPORT",
     "aop": 60,
     "lat": 42.0,
     "dop": 60,
     "lng": 2.0
-  },
+  }
+];
